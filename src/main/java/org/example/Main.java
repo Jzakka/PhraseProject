8c5799a511fcafbe11504a391b2adc6b24d13e1c
Jzakka <mouse4786@gmail.com>
2023-03-02 09:10:37 +0900

    feat: Level 1 clear

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 명언 앱 ==");
+
+        String command;
+        while (!(command = getCommand()).equals("종료")) {
+
+        }
+    }
+
+    static String getCommand() {
+        System.out.print("명령) ");
+        return sc.nextLine();
     }
 }
